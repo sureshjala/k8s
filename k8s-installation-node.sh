9b50df4010edcdaa170a0cda52ff781081620733
sureshjala <sureshjala7@gmail.com>
2022-12-17 12:48:49 +0530

    added script

---
+++ k8s-installation-node.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#run in root user
+sudo -i
+apt update
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+wget https://storage.googleapis.com/golang/getgo/installer_linux
+chmod +x ./installer_linux && ./installer_linux
+source ~/.bash_profile
+git clone https://github.com/Mirantis/cri-dockerd.git
+cd cri-dockerd
+mkdir bin
+go build -o bin/cri-dockerd
+mkdir -p /usr/local/bin
+install -o root -g root -m 0755 bin/cri-dockerd /usr/local/bin/cri-dockerd
+cp -a packaging/systemd/* /etc/systemd/system
+sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+systemctl daemon-reload
+systemctl enable cri-docker.service 
+systemctl enable --now cri-docker.socket
+systemctl status cri-docker.socket
+
+exit
+
+sudo apt update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update 
+sudo apt-get install -y kubelet=1.25.0-00 kubeadm=1.25.0-00 kubectl=1.25.0-00
+sudo apt-mark hold kubelet kubeadm kubectl
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+sudo apt update 
+
+sudo apt install openjdk-11-jdk -y
